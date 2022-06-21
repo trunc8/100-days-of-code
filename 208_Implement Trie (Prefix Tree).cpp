f69c77fc09d5cae2a4b6fa58455985f83d0f82f0
Siddharth Saha <sahasiddharth611@gmail.com>
2022-06-21 17:04:54 +0530

    #208: Smarter solution using fixed-size array and using index number to infer character

--- 208_Implement Trie (Prefix Tree).cpp
+++ 208_Implement Trie (Prefix Tree).cpp
@@ -79,6 +79,56 @@ public:
     }
 };
 
+
+// Solution 2: Using 26 size array and using index position to infer letter
+struct Node{
+    shared_ptr<Node> nodeptr[26];
+    bool is_end_of_word;
+};
+
+class Trie {
+private:
+    shared_ptr<Node> root;
+public:
+    Trie() {
+        root = shared_ptr<Node>(new Node());
+    }
+    
+    void insert( string word ) {
+        shared_ptr<Node> curr_node = root;
+        for( auto w : word ) {
+            if( !curr_node->nodeptr[ w-'a' ] ) {
+                curr_node->nodeptr[ w-'a' ] = shared_ptr<Node>( 
+                    new Node() );
+                curr_node->nodeptr[ w-'a' ]->is_end_of_word = false;
+            }
+            curr_node = curr_node->nodeptr[ w-'a' ];
+        }
+        curr_node->is_end_of_word = true;        
+    }
+    
+    bool search( string word ) {
+        shared_ptr<Node> curr_node = root;
+        for( auto w : word ) {
+            if( !curr_node->nodeptr[ w-'a' ] )
+                return false;
+            curr_node = curr_node->nodeptr[ w-'a' ];
+        }
+        return curr_node->is_end_of_word;
+    }
+    
+    bool startsWith( string prefix ) {
+        shared_ptr<Node> curr_node = root;
+        for( auto w : prefix ) {   
+            if( !curr_node->nodeptr[ w-'a' ] )
+                return false;
+            curr_node = curr_node->nodeptr[ w-'a' ];
+        }
+        return true;
+    }
+};
+
+
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie* obj = new Trie();
