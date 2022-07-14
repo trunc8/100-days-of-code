d8ddbf34b766150ec67698fe16208f811c244f5e
Siddharth Saha <sahasiddharth611@gmail.com>
2022-07-14 11:48:11 +0530

    Added #105 solution 2

--- Solutions/105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Solutions/105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Solution 1: Iterating to find node, didn't leverage "distinct" values in vector.
 class Solution {
 public:
     void buildLeftTree( TreeNode* root, const vector<int>& preorder, int& index, vector<int> inorder ){
@@ -68,5 +69,37 @@ public:
         buildLeftTree( root, preorder, index, left_inorder );
         buildRightTree( root, preorder, index, right_inorder );
         return soln;
+    }
+};
+
+
+// Solution 2: Heavily referred leetcode solution. Using hashmap. Work of art the more I see it
+class Solution {
+public:
+    unordered_map<int, int> um;
+    int index;
+    
+    TreeNode* build( const vector<int>& preorder, int left, int right ){
+        if( left > right ){
+            return NULL;
+        }
+        TreeNode* root;
+        int root_value = preorder.at(index++);
+        root = new TreeNode( root_value );
+
+        root->left = build( preorder, left, um[ root_value ]-1 );
+        root->right = build( preorder, um[ root_value ]+1, right );        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* soln = new TreeNode( preorder.front() );
+        TreeNode* root = soln;
+        int index = 0;
+        int boundary;
+        for( int i=0; i<inorder.size(); i++ ){
+            um[ inorder.at(i) ] = i;
+        }
+        return build( preorder, 0, inorder.size()-1 );;
     }
 };
